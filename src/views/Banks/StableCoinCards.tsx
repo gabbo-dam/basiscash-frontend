49c24c7f8d28982c73d36ddb726bc35076d36e86
Gabriel Morales <gabbomorales1@gmail.com>
2021-01-18 13:39:57 -0600

    (StableCoinCards) Code optimized

--- src/views/Banks/StableCoinCards.tsx
+++ src/views/Banks/StableCoinCards.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Bank } from '../../basis-cash';
 import Button from '../../components/Button';
 import Card from '../../components/Card';
 import CardContent from '../../components/CardContent';
 import CardIcon from '../../components/CardIcon';
-import useBanks from '../../hooks/useBanks';
-import TokenSymbol from '../../components/TokenSymbol';
-import Notice from '../../components/Notice';
-import BankCards from './BankCards';
+
 
 const StableCoinCard = () => {
   return (
@@ -33,91 +29,6 @@ const StableCoinCard = () => {
   )
 }
 
-const StyledCardAccent = styled.div`
-  background: linear-gradient(
-    45deg,
-    rgba(255, 0, 0, 1) 0%,
-    rgba(255, 154, 0, 1) 10%,
-    rgba(208, 222, 33, 1) 20%,
-    rgba(79, 220, 74, 1) 30%,
-    rgba(63, 218, 216, 1) 40%,
-    rgba(47, 201, 226, 1) 50%,
-    rgba(28, 127, 238, 1) 60%,
-    rgba(95, 21, 242, 1) 70%,
-    rgba(186, 12, 248, 1) 80%,
-    rgba(251, 7, 217, 1) 90%,
-    rgba(255, 0, 0, 1) 100%
-  );
-  border-radius: 12px;
-  filter: blur(4px);
-  position: absolute;
-  top: -2px;
-  right: -2px;
-  bottom: -2px;
-  left: -2px;
-  z-index: -1;
-`;
-
-const StyledCardSuperAccent = styled.div`
-  background: linear-gradient(
-    45deg,
-    rgba(255, 0, 0, 1) 0%,
-    rgba(255, 154, 0, 1) 10%,
-    rgba(208, 222, 33, 1) 20%,
-    rgba(79, 220, 74, 1) 30%,
-    rgba(63, 218, 216, 1) 40%,
-    rgba(47, 201, 226, 1) 50%,
-    rgba(28, 127, 238, 1) 60%,
-    rgba(95, 21, 242, 1) 70%,
-    rgba(186, 12, 248, 1) 80%,
-    rgba(251, 7, 217, 1) 90%,
-    rgba(255, 0, 0, 1) 100%
-  );
-  border-radius: 12px;
-  filter: blur(8px);
-  position: absolute;
-  top: -4px;
-  right: -4px;
-  bottom: -4px;
-  left: -4px;
-  z-index: -1;
-`;
-
-const StyledCards = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 900px;
-  @media (max-width: 768px) {
-    width: 100%;
-  }
-`;
-
-const StyledLoadingWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex: 1;
-  justify-content: center;
-`;
-
-const StyledRow = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-bottom: ${(props) => props.theme.spacing[4]}px;
-  flex-flow: row wrap;
-  @media (max-width: 768px) {
-    width: 100%;
-    flex-flow: column nowrap;
-    align-items: center;
-  }
-`;
-
-const StyledCardWrapper = styled.div`
-  display: flex;
-  width: calc((900px - ${(props) => props.theme.spacing[4]}px * 2) / 3);
-  position: relative;
-`;
-
 const StyledTitle = styled.h4`
   color: ${(props) => props.theme.color.grey[200]};
   font-size: 24px;
@@ -133,11 +44,6 @@ const StyledContent = styled.div`
   flex-direction: column;
 `;
 
-const StyledSpacer = styled.div`
-  height: ${(props) => props.theme.spacing[4]}px;
-  width: ${(props) => props.theme.spacing[4]}px;
-`;
-
 const StyledDetails = styled.div`
   margin-bottom: ${(props) => props.theme.spacing[6]}px;
   margin-top: ${(props) => props.theme.spacing[2]}px;
@@ -146,19 +52,6 @@ const StyledDetails = styled.div`
 
 const StyledDetail = styled.div`
   color: ${(props) => props.theme.color.grey[300]};
-`;
-
-const StyledInactiveNoticeContainer = styled.div`
-  width: 598px;
-  margin-bottom: ${(props) => props.theme.spacing[6]}px;
-`;
-
-const StyledInactiveBankTitle = styled.p`
-  font-size: 24px;
-  font-weight: 600;
-  color: ${(props) => props.theme.color.grey[400]};
-  margin-top: ${(props) => props.theme.spacing[5]}px;
-  margin-bottom: ${(props) => props.theme.spacing[4]}px;
 `;
 
 export default StableCoinCard
